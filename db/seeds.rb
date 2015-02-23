175324a3c75cca0dc5453b0423a3d53cf4c62327
Mertak <mertak@bil.omu.edu.tr>
2015-02-23 03:00:43 +0200

    Admins Unregistrable

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Admin.create(email: 'admin@admin.com', password: "12345678", password_confirmation: "12345678")
